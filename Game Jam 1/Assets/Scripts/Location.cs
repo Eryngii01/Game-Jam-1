7a875577dd95481d06f1ce904bc255c36a1b97ec
Lev McKinney <levmckinney@gmail.com>
2022-09-17 09:46:34 -0700

    add script for the location

---
+++ Game Jam 1/Assets/Scripts/Location.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LocationState
+{
+    Okay,
+    Disaster,
+    Recovering,
+}
+
+
+public class Location : MonoBehaviour
+{
+    public LocationState locationState;
+    public float recoveryTime;
+    private const string moonShadowTag = "MoonShadow";
+    private float _recoveryTimer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        locationState = LocationState.Okay;
+    }
+
+    void Update()
+    {
+        if (locationState == LocationState.Recovering)
+        {
+            _recoveryTimer -= Time.deltaTime;
+            if (_recoveryTimer <= 0)
+            {
+                locationState = LocationState.Okay;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(moonShadowTag) && locationState == LocationState.Disaster)
+        {
+            _recoveryTimer = recoveryTime;
+
+            Debug.Log("Moon shadow hit shadow.");
+            locationState = LocationState.Recovering;
+        }
+    }
+}
